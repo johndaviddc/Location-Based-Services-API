c921ae2dd56666655480273f054c9b95ef92b57a
John David Dela Cruz <johndaviddc1@gmail.com>
2024-01-08 12:37:49 +0800

    Location-Based Services API

---
+++ src/main/java/dave/dev/locationservices/LocationservicesApplication.java
@@ -0,0 +1,13 @@
+package dave.dev.locationservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dave/dev/locationservices/LocationservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.dev.locationservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
