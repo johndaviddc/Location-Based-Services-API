aaf1d31e0e576a5e7be031f042bb3c4f2002ed17
John David Dela Cruz <johndaviddc1@gmail.com>
2024-01-09 14:22:28 +0800

    added getter and setter

--- src/main/java/dave/dev/locationservices/model/Location.java
+++ src/main/java/dave/dev/locationservices/model/Location.java
@@ -21,5 +21,35 @@ public class Location {
         this.longitude = longitude;
     }
 
-    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
