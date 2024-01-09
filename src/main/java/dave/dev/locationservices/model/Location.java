d66c019450e046e8c71cea07116d4e8de5c4f4f3
John David Dela Cruz <johndaviddc1@gmail.com>
2024-01-09 14:21:55 +0800

    added model package

---
+++ src/main/java/dave/dev/locationservices/model/Location.java
@@ -0,0 +1,25 @@
+package dave.dev.locationservices.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double latitude;
+    private double longitude;
+
+    public Location(Long id, String name, double latitude, double longitude) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    
+}
