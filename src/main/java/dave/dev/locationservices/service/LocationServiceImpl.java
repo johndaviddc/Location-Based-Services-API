ec0d7a406b8ecbb9736296bd6c0cf0eb43a46f98
John David Dela Cruz <johndaviddc1@gmail.com>
2024-01-12 12:50:03 +0800

    added location service implementation

--- src/main/java/dave/dev/locationservices/service/LocationServiceImpl.java
+++ src/main/java/dave/dev/locationservices/service/LocationServiceImpl.java
@@ -1,5 +1,17 @@
 package dave.dev.locationservices.service;
 
+import dave.dev.locationservices.model.Location;
+import dave.dev.locationservices.repository.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class LocationServiceImpl implements LocationService {
-    
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @Override
+    public List<Location> getAllLocations() {
+        return locationRepository.findAll();
+    }
 }
