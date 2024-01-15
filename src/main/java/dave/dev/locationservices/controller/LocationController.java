08147455c325d36185cdde644b54b8dea6bbbe6f
John David Dela Cruz <johndaviddc1@gmail.com>
2024-01-15 09:16:27 +0800

    added controller package

---
+++ src/main/java/dave/dev/locationservices/controller/LocationController.java
@@ -0,0 +1,29 @@
+package dave.dev.locationservices.controller;
+
+import dave.dev.locationservices.model.Location;
+import dave.dev.locationservices.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/locations")
+public class LocationController {
+
+    @Autowired
+    private LocationService locationService;
+
+    @GetMapping
+    public List<Location> getAllLocations() {
+        return locationService.getAllLocations();
+    }
+
+    @GetMapping("/{id}")
+    public Location getLocationById(@PathVariable Long id) {
+        return locationService.getLocationById(id);
+    }
+}
